48724c67766aa0eff64658868d45dada2296e4fc
AngeloCG97 <angelocas13_8@hotmail.com>
2021-05-07 16:33:52 -0600

    new(design): apply new design to not found page

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -29,7 +29,7 @@ function NotFound() {
               <Box className="section">
                 <Box className="titleBox">
                     <h1 style={{paddingLeft: '25px', paddingRight:'25px', fontSize:'30px'}} className="centerText">¡Ups! No pudimos encontrar esta página.</h1>
-                    <h2 style={{paddingLeft: '25px', paddingRight:'25px', fontSize:'20px'}} className="centerText">Por favor, regrese a la <a style={{color:'#1dc961'}} href="/">página de inicio</a> e inténtalo de nuevo.</h2>
+                    <h2 style={{paddingLeft: '25px', paddingRight:'25px', fontSize:'20px'}} className="centerText">Por favor, regrese a la <a style={{color:'#1dc961'}} href="/">página de inicio</a> e inténtelo de nuevo.</h2>
                   </Box>
                   <p style={{paddingLeft: '25px', paddingRight:'25px',fontSize:'14px'}} className="centerText">Si tiene alguna duda sobre LACChain EOSIO diríjase a <a href="/docs/testnet/eosio">nuestra documentación</a>!</p>
                 </Box>
